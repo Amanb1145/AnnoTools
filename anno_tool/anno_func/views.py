0fe5933800c6ddb67203ba70f802a4a10f57a9eb
aman <amanb1145@gmail.com>
2021-03-01 20:29:43 +0530

    updated views

--- anno_tool/anno_func/views.py
+++ anno_tool/anno_func/views.py
@@ -9,25 +9,19 @@ from .serializer import AnnoSerializer
 class anno_tool(APIView):
 
     def get(self, request):
-        file = AnnoTool.objects.filter(resume=request.GET.get("resume"))
-        fileserializer = AnnoSerializer(file)
-        print(fileserializer.data)
-        name = AnnoTool.objects.filter(name=request.GET.get("name"))
-        #nameserializer = AnnoSerializer(name)
-        degree = AnnoTool.objects.filter(degree=request.GET.get("degree"))
-        #degreeserializer = AnnoSerializer(degree)
-        college = AnnoTool.objects.filter(college=request.GET.get("college"))
-        #collegeserializer = AnnoSerializer(college)
-        organization = AnnoTool.objects.filter(organization=request.GET.get("organization"))
-        #organizationserializer = AnnoSerializer(organization)
-        designation = AnnoTool.objects.filter(designation=request.GET.get("designation"))
-        #designationserializer = AnnoSerializer(designation)
-        date_of_birth = AnnoTool.objects.filter(date_of_birth=request.GET.get("date_of_birth"))
-        #date_of_birthserializer = AnnoSerializer(date_of_birth)
-        summary = AnnoTool.objects.filter(summary=request.GET.get("summary"))
-        #summaryserializer = AnnoSerializer(summary)
-        ents = [name, degree, college, organization, designation, date_of_birth, summary]
-        return return_annotated_file(file, ents)
+        data = {
+            "resume": AnnoTool.objects.filter(resume=request.GET.get("resume")),
+            "name": AnnoTool.objects.filter(name=request.GET.get("name")),
+            "degree": AnnoTool.objects.filter(degree=request.GET.get("degree")),
+            "college": AnnoTool.objects.filter(college=request.GET.get("college")),
+            "organization": AnnoTool.objects.filter(organization=request.GET.get("organization")),
+            "designation": AnnoTool.objects.filter(designation=request.GET.get("designation")),
+            "date_of_birth": AnnoTool.objects.filter(date_of_birth=request.GET.get("date_of_birth")),
+            "summary": AnnoTool.objects.filter(summary=request.GET.get("summary"))
+        }
+        dataserializer = AnnoSerializer(data)
+
+        return return_annotated_file(data.get("resume"), dict(list(data.items())[1:]))
 
 
 
